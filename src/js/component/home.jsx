9046258accb95488bcbd4f5bf4d78f85c601e805
sibelius303 <cesarev17@gmail.com>
2022-09-10 23:58:38 +0000

    version final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
+import propTypes from "prop-types";
 //include images into your bundle
 
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="contenedor">
 			<div className="cardCounter"><i class="fa-regular fa-clock"></i></div>
-			<div className="cardCounter">0</div>
-			<div className="cardCounter">0</div>
-			<div className="cardCounter">0</div>
-			<div className="cardCounter">0</div>
-			<div className="cardCounter">0</div>
+			<div className="cardCounter">{props.digiFive %10}</div>
+			<div className="cardCounter">{props.digiFour %10}</div>
+			<div className="cardCounter">{props.digiThree %10}</div>
+			<div className="cardCounter">{props.digiTwo %10}</div>
+			<div className="cardCounter">{props.digiOne %10}</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -8,5 +9,27 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+Home.propTypes = {
+    digiFive: propTypes.number,
+    digiFour: propTypes.number,
+    digiThree: propTypes.number,
+    digiTwo: propTypes.number,
+    digiOne: propTypes.number,
+}
+let counter = 0;
+
+setInterval(function (){
+    const five = Math.floor(counter/10000);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter++;
+    //render your react application
+    ReactDOM.render(<Home digiOne={one} digiTwo={two} digiThree={three} digiFour={four} digiFive={five}/>,
+     document.querySelector("#app")
+    );
+},1000)
+
+
