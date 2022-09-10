636a4a64f8c22cef7c3fe90eeea4cb996d3a5d3a
sibelius303 <cesarev17@gmail.com>
2022-09-10 23:32:01 +0000

    Estilos terminados, falta logica

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import CounterCard from "./counterCard.jsx";
-import CounterIcono from "./counterIcono.jsx"
+
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="contenedor">
-			<CounterIcono/>
-			<CounterCard/>
-			<CounterCard/>
-			<CounterCard/>
-			<CounterCard/>
-			<CounterCard/>
-			<CounterCard/>
+			<div className="cardCounter"><i class="fa-regular fa-clock"></i></div>
+			<div className="cardCounter">0</div>
+			<div className="cardCounter">0</div>
+			<div className="cardCounter">0</div>
+			<div className="cardCounter">0</div>
+			<div className="cardCounter">0</div>
 		</div>
 	);
 };
